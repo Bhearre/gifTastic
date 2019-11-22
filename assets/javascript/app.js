54e170e3f217acbecfd7a4c2e5ddb7393ddfa363
George Ferguson <george@fergusondevelopmentgroup.com>
2019-11-22 10:19:58 -0500

    Basic Framework for project gifTastic using movie exercise for intial js functionality and initial styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+
+      // Function for displaying movie data
+      function renderButtons() {
+
+        // Deleting the movie buttons prior to adding new movie buttons
+        // (this is necessary otherwise we will have repeat buttons)
+        $("#buttons-view").empty();
+
+        // Looping through the array of movies
+        for (var i = 0; i < movies.length; i++) {
+
+          // Then dynamicaly generating buttons for each movie in the array.
+          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+          var a = $("<button>");
+          // Adding a class
+          a.addClass("movie");
+          // Adding a data-attribute with a value of the movie at index i
+          a.attr("data-name", movies[i]);
+          // Providing the button's text with a value of the movie at index i
+          a.text(movies[i]);
+          // Adding the button to the HTML
+          $("#buttons-view").append(a);
+        }
+      }
+    
+    renderButtons();
