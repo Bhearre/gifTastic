f93a5960febba6ce5a132c026499b4ca1e500a7d
George Ferguson <george@fergusondevelopmentgroup.com>
2019-11-23 16:36:03 -0500

    Push of the gitTastic app in its final form

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # gifTastic
-A new repository for the gifTastic project HW 6
+gifTastic is a cure for all that ails you. A frolic down cartoon lane with your childhood favorites. Pick one of the existing cartoon characters or add your own.
+
+On click you will be rewarded with 10 stills of the selected character and clicking the character will bring them to life.
+
+The technologies employed here are:
+* html
+* javascript
+* jQuery
+* css
+* bootstrap
+
+Have fun and have a great day!
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,24 @@
-var animals = ["Cats", "Dogs", "Hamsters", "Ferrets"];
+var cartoons = ["Elmer Fudd", "Tweety Bird", "Foghorn Leghorn", "Road Runner", "Wile E. Coyote", "Daffy Duck", "Marvin the Martian", "Pinky and the Brain", "Fred Flintstone", "George Jetson", "Tasmanian Devil"];
 
-// Function for displaying animal data
+// Function for displaying cartoon data
 function renderButtons() {
 
-    // Deleting the animals buttons prior to adding new animal buttons
+    // Deleting the cartoons buttons prior to adding new cartoon buttons
     // (this is necessary otherwise we will have repeat buttons)
     $("#buttons-view").empty();
 
-    // Looping through the array of animals
-    for (var i = 0; i < animals.length; i++) {
+    // Looping through the array of cartoons
+    for (var i = 0; i < cartoons.length; i++) {
 
-        // Then dynamicaly generating buttons for each animal in the array.
+        // Then dynamicaly generating buttons for each cartoon in the array.
         // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
         var a = $("<button>");
         // Adding a class
-        a.addClass("animal");
+        a.addClass("cartoon");
         // Adding a data-attribute with a value of the movie at index i
-        a.attr("data-name", animals[i]);
+        a.attr("data-name", cartoons[i]);
         // Providing the button's text with a value of the movie at index i
-        a.text(animals[i]);
+        a.text(cartoons[i]);
         a.on("click", loadGifs);
         // Adding the button to the HTML
         $("#buttons-view").append(a);
@@ -28,30 +28,30 @@ renderButtons();
 
 
 // This .on("click") function will trigger the AJAX Call
-$("#add-animal").on("click", function (event) {
+$("#add-cartoon").on("click", function (event) {
 
     // event.preventDefault() can be used to prevent an event's default behavior.
     // prevents the submit button from trying to submit a form when clicked
     event.preventDefault();
 
     // Here we grab the text from the input box
-    var animal = $("#animal-input").val().trim();
-    if(animal === ""){
+    var cartoon = $("#cartoon-input").val().trim();
+    if(cartoon === ""){
         return;
     }
     else {
-    animals.push(animal);
-    console.log(animals);
+    cartoons.push(cartoon);
+    console.log(cartoons);
     renderButtons();
-    $("#animal-input").val("");
+    $("#cartoon-input").val("");
 };
 });
 
 function loadGifs(event) {
-    var animal = $(this).attr("data-name");
-    console.log(animal);
+    var cartoon = $(this).attr("data-name");
+    console.log(cartoon);
     // encodeURI translates spaces in user input into readable string
-    var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=iJpeCQHCcdk058xQ6eV06LW74SSChwfp&q=" + encodeURI(animal) + "&limit=10&rating=G&lang=en";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=iJpeCQHCcdk058xQ6eV06LW74SSChwfp&q=" + encodeURI(cartoon) + "&limit=10&rating=G&lang=en";
     console.log(queryURL);
     $.ajax({
         url: queryURL,
@@ -60,29 +60,29 @@ function loadGifs(event) {
 
         var results = response.data;
 
-        $("#animals-view").empty();
+        $("#cartoons-view").empty();
 
         // Looping through each result item
         for (var i = 0; i < results.length; i++) {
 
             // Creating and storing a div tag
-            var animalDiv = $("<div>");
+            var cartoonDiv = $("<div>");
 
             // Creating a paragraph tag with the result item's rating
             var p = $("<p>").text("Rating: " + results[i].rating);
 
             // Creating and storing an image tag
-            var animalImage = $("<img>");
+            var cartoonImage = $("<img>");
             // Setting the src attribute of the image to a property pulled off the result item
-            // animalImage.attr("src", results[i].images.fixed_height.url);
-            animalImage.attr("src", results[i].images.fixed_height_still.url);
-            animalDiv.addClass("float-md-left")
-            // Appending the paragraph and image tag to the animalDiv
-            animalDiv.append(p);
-            animalDiv.append(animalImage);
-
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-            $("#animals-view").prepend(animalDiv);
+            // cartoonImage.attr("src", results[i].images.fixed_height.url);
+            cartoonImage.attr("src", results[i].images.fixed_height_still.url);
+            cartoonDiv.addClass("float-md-left")
+            // Appending the paragraph and image tag to the cartoonDiv
+            cartoonDiv.append(cartoonImage);
+            cartoonDiv.append(p);
+            
+            // Prependng the cartoonDiv to the HTML page in the "#gifs-appear-here" div
+            $("#cartoons-view").prepend(cartoonDiv);
         }
 
         console.log(response.data);
